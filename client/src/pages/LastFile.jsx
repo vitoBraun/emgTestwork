5953284d8fb6b613d671bc9454b8e49d980f02eb
Vitlaii Braun <vbraun@team.amocrm.com>
2022-04-25 16:49:57 +0300

    ok

--- client/src/pages/LastFile.jsx
+++ client/src/pages/LastFile.jsx
@@ -23,7 +23,7 @@ export const LastFile = () => {
   return (
     <>
       <Container style={{ marginTop: "20px" }}>
-        Cсылка: <a href={lastFile.link}>{lastFile.link}</a> <br />
+        Cсылка на JSON: <a href="/api/lastfile">/api/lastfile</a> <br />
         <img src={lastFile.link} alt={lastFile.text} />
         <br />
         Создан: {lastFile.created_at}
